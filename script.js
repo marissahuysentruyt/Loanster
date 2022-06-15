f78c270d8365410c55f0e36727f17f6d1c83005c
Marissa Huysentruyt <marissahuysentruyt@gmail.com>
2022-06-15 16:43:16 -0400

    added tests
    added some random numbers to principal, rate, and payment variables to test math.
    
    deleted parameters from functions

--- script.js
+++ script.js
@@ -3,34 +3,43 @@ let principal; //principal loan amount; input by user
 let rate; //interest rate; selected by user
 let numberOfPayments; //number of payments; selected by user
 
-principal = 000
-rate = 12
-numberOfPayments = 84
+// tests
+// principal = 60000
+// rate = 12
+// numberOfPayments = 84
+
+// principal = 10000
+// rate = 6.8
+// numberOfPayments = 60
+
+principal = 250000
+rate = 2.84
+numberOfPayments = 360
 
 
 //this function changes the rate into a decimal, then divide by 12 to get a monthly interest rate
-let monthlyRate = (rate) => {
+let monthlyRate = () => {
     let monthlyRate = (rate/100) / 12;
     return monthlyRate;
 }
-console.log(monthlyRate(rate))
+// console.log(monthlyRate())
 
 
 //the top figure in the equation uses the rate (monthly interest rate) and the numberOfPayments. Add 1 to the rate, and exponentiate that by the numberOfPayments. Multiply that exponential number by the rate again.
-let topFigure = (monthlyRate(rate) * ((1 + monthlyRate(rate)) ** numberOfPayments));
+let topFigure = (monthlyRate() * ((1 + monthlyRate()) ** numberOfPayments));
 //the bottom figure in the equation uses the rate (monthly) and numberOfPayments again. Add 1 to the rate, exponentiate that by the numberOfPayments. Subtract one. 
-let bottomFigure = (((1 + monthlyRate(rate)) ** numberOfPayments) - 1);
-console.log(topFigure)
-console.log(bottomFigure)
+let bottomFigure = (((1 + monthlyRate()) ** numberOfPayments) - 1);
+// console.log(topFigure)
+// console.log(bottomFigure)
 
 //this function takes the principal, topFigure and bottomFigure, returning a monthly payment amount. 
-const loanCalculation = (principal, topFigure, bottomFigure) => {
+const loanCalculation = () => {
     //calculate the monthly payment...divide the top figure by the bottom figure, then multiply the answer by the principal 
     let monthlyAmount = (principal * (topFigure/bottomFigure));
     monthlyAmount = monthlyAmount.toFixed(2)
     return monthlyAmount;
 }
-console.log(`Monthly Payment is ${loanCalculation(principal, topFigure, bottomFigure)}.`)
+console.log(`Monthly Payment is ${loanCalculation()}.`)
 
 
 
