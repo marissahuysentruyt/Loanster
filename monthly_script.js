79b003c87166e748e403d10ccafa7da1258111ca
Marissa Huysentruyt <marissahuysentruyt@gmail.com>
2022-06-21 11:00:40 -0400

    file name changes (monthly fix)
    script file names were changed to be clearer (duration script, monthly script, initial script)
    
    I also fixed the monthlyLoanCalculation() to accurately grab the top/bottomFigures

---
+++ monthly_script.js
@@ -0,0 +1,82 @@
+//THIS IS ONLY FOR THE MONTHLY PAYMENT TOGGLE CALCULATION!!!!!!!!!!!!!!!!!!!!
+
+//principal loan amount; input by user
+let principal = document.getElementById("principal")
+principal = 
+    principal.addEventListener("keyup", function(event) {
+        principal = event.target.value;
+        principal = Number(principal)
+        console.log(`user entered ${principal}`)
+        return principal;  
+    })
+
+//interest rate; selected by user
+let rate = document.getElementById("interest-rate"); 
+rate = 
+    rate.addEventListener("mouseup", function(e) {
+    rate = e.target.value
+    rate = Number(rate)
+    console.log(`user selected ${rate}`)
+    return rate;
+})
+//number of payments; selected by user
+let numberOfPayments; 
+
+// tests
+// principal = 60000
+// rate = 12
+// numberOfPayments = 84
+
+// principal = 10000
+// rate = 6.8
+// numberOfPayments = 60
+
+// principal = 250000
+// rate = 2.84
+// numberOfPayments = 360
+
+
+//this function changes the rate into a decimal, then divide by 12 to get a monthly interest rate
+let monthlyInterestRate = () => {
+    let monthlyRate = (rate/100) / 12;
+    return monthlyRate;
+}
+// test monthlyInterestRate
+// console.log(monthlyInterestRate())
+
+
+
+
+//this function takes the principal, topFigure and bottomFigure, returning a monthly payment amount. 
+const monthlyLoanCalculation = () => {
+    //calculate the monthly payment...divide the top figure by the bottom figure, then multiply the answer by the principal 
+
+        // if(monthlyPaymentButton.classList.contains("active")) {
+        monthlyInterestRate();
+        //the top figure in the equation uses the rate (monthly interest rate) and the numberOfPayments. Add 1 to the rate, and exponentiate that by the numberOfPayments. Multiply that exponential number by the rate again.
+        let topFigure = (monthlyInterestRate() * ((1 + monthlyInterestRate()) ** numberOfPayments));
+        //the bottom figure in the equation uses the rate (monthly) and numberOfPayments again. Add 1 to the rate, exponentiate that by the numberOfPayments. Subtract one. 
+        let bottomFigure = (((1 + monthlyInterestRate()) ** numberOfPayments) - 1);
+        // test top/bottom figures
+        // console.log(topFigure)
+        // console.log(bottomFigure)
+
+        //the monthlyAmount takes the principal and multiplies by the topFigure divided by the bottomFigure
+        let monthlyAmount = (principal * (topFigure/bottomFigure));
+        //stop the decimal at 2 places
+        monthlyAmount = monthlyAmount.toFixed(2)
+        // return the monthlyAmount
+        return monthlyAmount;
+    } 
+    // else if (loanDurationButton.classList.contains("active")) {
+    //     console.log("not ready yet")
+    // }
+
+    // } 
+
+
+// console.log(`Monthly Payment is ${monthlyLoanCalculation()}.`)
+
+
+
+
--- script.js
+++ script.js
@@ -1,76 +1,136 @@
-//THIS IS ONLY FOR THE MONTHLY PAYMENT TOGGLE CALCULATION!!!!!!!!!!!!!!!!!!!!
+//BLUE COLUMN ONLY!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+//h3 buttons should change h1 text question and the input fields
 
-//principal loan amount; input by user
-let principal = document.getElementById("principal")
-principal = 
-    principal.addEventListener("keyup", function(event) {
-        principal = event.target.value;
-        principal = Number(principal)
-        console.log(`user entered ${principal}`)
-        return principal;  
-    })
+// monthlyPaymentButton defaults to active
+let monthlyPaymentButton = document.querySelector("h3.monthly");
+//loanDurationButton defaults to inactive
+let loanDurationButton = document.querySelector("h3.duration") 
 
-//interest rate; selected by user
-let rate = document.getElementById("interest-rate"); 
-rate = 
-    rate.addEventListener("mouseup", function(e) {
-    rate = e.target.value
-    rate = Number(rate)
-    console.log(`user selected ${rate}`)
-    return rate;
+// setting & removing classes so buttons change
+// loanDurationButton.setAttribute("class", "active")
+// loanDurationButton.removeAttribute("active")
+// loanDurationButton.setAttribute("class", "inactive")
+// loanDurationButton.removeAttribute("inactive")
+// monthlyPaymentButton.setAttribute("class", "active");
+// monthlyPaymentButton.removeAttribute("active");
+// monthlyPaymentButton.setAttribute("class", "inactive");
+// monthlyPaymentButton.removeAttribute("inactive");
+
+let monthlyQuestion = document.querySelector("h1.active");
+let durationQuestion = document.querySelector("h1.inactive");
+
+// setting & removing classes so h1 changes
+// durationQuestion.setAttribute("class", "active") 
+// durationQuestion.removeAttribute("active") 
+// durationQuestion.setAttribute("class", "inactive") 
+// durationQuestion.removeAttribute("inactive") 
+// monthlyQuestion.setAttribute("class", "active") 
+// monthlyQuestion.removeAttribute("active")
+// monthlyQuestion.setAttribute("class", "inactive") 
+// monthlyQuestion.removeAttribute("inactive") 
+
+
+
+
+
+
+//INPUT COLUMN ONLY!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+//dropdown menu, monthly payment input, outputs
+//dropdown defaults to active
+let dropdown = document.querySelector(" p.input-duration.active ");
+//preferredMonthlyPayment defaults to inactive
+let preferredMonthlyPayment = document.querySelector("p.input-monthly-payment.inactive")
+//monthlyPaymentOutput defaults to active
+let monthlyPaymentOutput = document.querySelector(".monthly-outputs.active")
+//loanDurationOutput defaults to inactive
+let loanDurationOutput = document.querySelector(".remaining-months.inactive")
+
+
+// setting & removing classes so input field changes
+// dropdown.setAttribute("class", "input-duration inactive")
+// dropdown.classList.remove("inactive")
+// dropdown.setAttribute("class", "input-duration active")
+// dropdown.classList.remove("active")
+// preferredMonthlyPayment.setAttribute("class", "input-monthly-payment inactive")
+// preferredMonthlyPayment.classList.remove("inactive")
+// preferredMonthlyPayment.setAttribute("class", "input-monthly-payment active")
+// preferredMonthlyPayment.classList.remove("active")
+// monthlyPaymentOutput.setAttribute("class", "monthly-outputs active")
+// monthlyPaymentOutput.classList.remove("active")
+// monthlyPaymentOutput.setAttribute("class", "monthly-outputs inactive")
+// monthlyPaymentOutput.classList.remove("inactive")
+// loanDurationOutput.setAttribute("class", "remaining-months active")
+// loanDurationOutput.classList.remove("active")
+// loanDurationOutput.setAttribute("class", "remaining-months inactive")
+// loanDurationOutput.classList.remove("inactive")
+
+
+
+
+
+
+
+//toggle calculation buttons
+monthlyPaymentButton.addEventListener("click", function() {
+    // test: monthlyPaymentButton.style.border = "1px solid red"
+    // test: monthlyQuestion.innerText = "changed"
+
+    // correct button & h1 are displayed
+    monthlyPaymentButton.setAttribute("class", "active");
+    monthlyPaymentButton.removeAttribute("inactive");
+    monthlyQuestion.removeAttribute("inactive")
+    monthlyQuestion.setAttribute("class", "active"); 
+    // loanDurationButton & durationQuestion should be "off"
+    durationQuestion.removeAttribute("active")
+    durationQuestion.setAttribute("class", "inactive")
+    loanDurationButton.removeAttribute("active")
+    loanDurationButton.setAttribute("class", "inactive")
+
+    //change input fields
+    //dropdown is on, prefMonth... is off
+    dropdown.classList.remove("inactive")
+    dropdown.setAttribute("class", "input-duration active")
+    preferredMonthlyPayment.classList.remove("active")
+    preferredMonthlyPayment.setAttribute("class", "input-monthly-payment inactive")
+
+    //change output
+    monthlyPaymentOutput.setAttribute("class", "monthly-outputs active")
+    monthlyPaymentOutput.classList.remove("inactive")
+    loanDurationOutput.classList.remove("active")
+    loanDurationOutput.setAttribute("class", "remaining-months inactive")
+
 })
-//number of payments; selected by user
-let numberOfPayments; 
 
-// tests
-// principal = 60000
-// rate = 12
-// numberOfPayments = 84
+loanDurationButton.addEventListener("click", function() {
+    // test: loanDurationButton.style.border = "1px solid red"
 
-// principal = 10000
-// rate = 6.8
-// numberOfPayments = 60
-
-// principal = 250000
-// rate = 2.84
-// numberOfPayments = 360
-
-
-//this function changes the rate into a decimal, then divide by 12 to get a monthly interest rate
-let monthlyInterestRate = () => {
-    let monthlyRate = (rate/100) / 12;
-    return monthlyRate;
-}
-// test monthlyInterestRate
-console.log(monthlyInterestRate())
-
-
-//the top figure in the equation uses the rate (monthly interest rate) and the numberOfPayments. Add 1 to the rate, and exponentiate that by the numberOfPayments. Multiply that exponential number by the rate again.
-let topFigure = (monthlyInterestRate() * ((1 + monthlyInterestRate()) ** numberOfPayments));
-//the bottom figure in the equation uses the rate (monthly) and numberOfPayments again. Add 1 to the rate, exponentiate that by the numberOfPayments. Subtract one. 
-let bottomFigure = (((1 + monthlyInterestRate()) ** numberOfPayments) - 1);
-// test top/bottom figures
-// console.log(topFigure)
-// console.log(bottomFigure)
-
-//this function takes the principal, topFigure and bottomFigure, returning a monthly payment amount. 
-const monthlyLoanCalculation = () => {
-    //calculate the monthly payment...divide the top figure by the bottom figure, then multiply the answer by the principal 
-
-    // if(monthlyPaymentButton.classList.contains("active")) {
-        let monthlyAmount = (principal * (topFigure/bottomFigure));
-        monthlyAmount = monthlyAmount.toFixed(2)
-        return monthlyAmount;
-    } 
-    // else if (loanDurationButton.classList.contains("active")) {
-    //     console.log("not ready yet")
-    // }
-
-    // } 
+    //correct button & h1 are displayed
+    durationQuestion.removeAttribute("inactive")
+    durationQuestion.setAttribute("class", "active") 
+    loanDurationButton.setAttribute("class", "active")
+    loanDurationButton.removeAttribute("inactive")
+    //monthlyPaymentButton & monthlyQuestion should be "off"
+    monthlyPaymentButton.removeAttribute("active")
+    monthlyPaymentButton.setAttribute("class", "inactive")
+    monthlyQuestion.removeAttribute("active")
+    monthlyQuestion.setAttribute("class", "inactive")
+    
+    // test: durationQuestion.innerText = "changed"
+    
+    //change input fields
+    //dropdown is off, prefMonth... is on
+    dropdown.classList.remove("active")
+    dropdown.setAttribute("class", "input-duration inactive")
+    preferredMonthlyPayment.classList.remove("inactive")
+    preferredMonthlyPayment.setAttribute("class", "input-monthly-payment active")
 
 
-console.log(`Monthly Payment is ${monthlyLoanCalculation()}.`)
-
-
+    //change output
+    monthlyPaymentOutput.setAttribute("class", "monthly-outputs inactive")
+    monthlyPaymentOutput.classList.remove("active")
+    loanDurationOutput.classList.remove("inactive")
+    loanDurationOutput.setAttribute("class", "remaining-months active")
+})
 
 
